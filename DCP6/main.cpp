8fef22f9d4ff224f0ee8e03173217ad3a3728bbb
Wojciech Kogut <wojciech_kogut@o2.pl>
2019-02-07 18:22:07 +0100

    Uraaa!

--- DCP6/main.cpp
+++ DCP6/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
 using namespace std;
+
 template<class T>
 class List
 {
@@ -12,14 +14,41 @@ class List
         T val;
     public:
         Node(T nval):val(nval){}
-        T get() const {return val;}
-        Node* next(Node* prev) const {return reinterpret_cast<Node*>(reinterpret_cast<uint32_t>(xptr)^reinterpret_cast<uint32_t>(prev));}
+        Node* next(Node* prev) const
+        {
+            uint64_t x = reinterpret_cast<uint64_t>(xptr)^reinterpret_cast<uint64_t>(prev);
+            if(!x)
+                throw std::logic_error("index out of range");
+            return reinterpret_cast<Node*>(x);
+        }
         void push(Node* ptr)
         {
-            xptr = reinterpret_cast<Node*>(reinterpret_cast<uint32_t>(xptr)^reinterpret_cast<uint32_t>(ptr));
-            ptr->xptr = reinterpret_cast<Node*>(reinterpret_cast<uint32_t>(ptr->xptr)^reinterpret_cast<uint32_t>(this));
+            xptr = reinterpret_cast<Node*>(reinterpret_cast<uint64_t>(xptr)^reinterpret_cast<uint64_t>(ptr));
+            ptr->xptr = reinterpret_cast<Node*>(reinterpret_cast<uint64_t>(ptr->xptr)^reinterpret_cast<uint64_t>(this));
         }
     };
+    
+    Node* geti(int i=-1)
+    {
+        Node* ptr = node;
+        try
+        {
+            Node* prev = nullptr;
+            while(i--)
+            {
+                Node* tmp = ptr;
+                ptr = ptr->next(prev);
+                prev = tmp;
+            }
+        }
+        catch(logic_error e)
+        {
+            if(i>=0)
+                throw e;
+        }
+        return ptr;
+    }
+
     Node *node;
 public:
     List(T init)
@@ -29,21 +58,32 @@ public:
 
     void Add(T nval)
     {
-        Node* ptr = node;
-        while((ptr->xptr!=ptr)&&(ptr->xptr!=nullptr))
-            ptr = ptr->next(ptr);
-        ptr->push(new Node(nval));
+        geti()->push(new Node(nval));
+    }
+
+    T get(unsigned int i)
+    {
+        return (geti(i))->val;
     }
 };
 
 int main()
 {
-    List<int> lst(1);
-    lst.Add(2);
-    lst.Add(3);
-    //lst.Add("L3");
-    //lst.Add("L4");
-    //lst.Add("L5");
+    List<string> lst("L0");
+    lst.Add("L1");
+    lst.Add("L2");
+    lst.Add("L3");
+    lst.Add("L4");
+    lst.Add("L5");
+    try
+    {
+        for(int i=0;i<20;i++)
+            cout << "lst[" << i << "] = " << lst.get(i) << endl;
+    }
+    catch (logic_error)
+    {
+        cout << "iaor" << endl;
+    }
 
     return 0;
 }
